164d24140ac5d8c4b5089f27b7ebbe3650f5bcd8
Stephen Widom <stephen.widom@strategic-i.com>
2017-08-31 23:25:15 -0400

    Update paypal form

--- wp-content/themes/togetherfordevelopment/footer.php
+++ wp-content/themes/togetherfordevelopment/footer.php
@@ -24,12 +24,15 @@
         <span class="designer">Website by <a href="http://stephenwidom.com/" target="_blank">Stephen Widom</a></span>
     </div>
 </footer>
+
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank" id="donate">
     <input type="hidden" name="cmd" value="_s-xclick">
+    <input type="hidden" name="lc" value="en_US">
     <input type="hidden" name="hosted_button_id" value="2BMYGK87J77LA">
     <input type="image" src="https://www.paypalobjects.com/en_US/DE/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
     <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
+
 <?php wp_footer(); ?>
 </body>
 </html>
